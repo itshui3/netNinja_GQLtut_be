77b9bd64c4fe71d98dc6db7a1969acb8ab7fde44
Hui <jeffrey.x.hui@gmail.com>
2020-12-12 00:11:28 -0800

    brings in express-graphql

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const graphqlHTTP = require('express-graphql')
 
 // express is a function that evalutes to an app
 const app = express()
+
+// this will receive a gql request
+// we use graphqlHTTP because it will understand the gql req
+app.use('/graphql', graphqlHTTP({
+    // takes in some options in this object
+    
+}))
 
 app.listen(4000, () => {
     console.log('listening on port 4000')
